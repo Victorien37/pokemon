9872b9c5cff9d4d7070ec0c481b5c2073f66b64a
Victorien <rodriguesvictorien37@gmail.com>
2022-09-24 12:29:21 +0200

    correction des erreurs lié à la gestion des maps

--- scripts/map.py
+++ scripts/map.py
@@ -44,13 +44,13 @@ class MapManager:
         self.current_map = "bonaugure" #current_map will be dynamic later when save is enable
         
         ### REGISTER MAP DOIT ETRE OBLIGATOIREMENT ETRE DECLARER DANS LA CLASSE MAP MANAGER ET NON DANS UN AUTRE FICHIER ###
-        # self.register_map("bonaugure", portals=[
-        #     Portal(from_world="bonaugure", origin_point="enter_my_home", target_world="my_home", teleport_point="spawn_player"),
-        #     Portal(from_world="bonaugure", origin_point="enter_rival_home", target_world="rival_home", teleport_point="spawn"),
-        #     Portal("bonaugure", "enter_route-201", "route-201", "spawn_route-201")
-        # ], npcs=[
-        #     NPC("electro", nb_points=2, dialogs=["Tout le monde part à l'aventure","et devient peu à peu adulte..."])
-        # ])
+        self.register_map("bonaugure", portals=[
+            # Portal(from_world="bonaugure", origin_point="enter_my_home", target_world="my_home", teleport_point="spawn_player"),
+            # Portal(from_world="bonaugure", origin_point="enter_rival_home", target_world="rival_home", teleport_point="spawn"),
+            # Portal("bonaugure", "enter_route-201", "route-201", "spawn_route-201")
+        ], npcs=[
+            NPC("electro", nb_points=2, dialogs=["Tout le monde part à l'aventure","et devient peu à peu adulte..."])
+        ])
         
         self.teleport_player("player")
         self.teleport_npcs()
